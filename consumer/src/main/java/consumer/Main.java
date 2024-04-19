73532828ac7c86d713a6078e26d779478f67937c
Robin Alfengård <robin.alfengard@hotmail.com>
2024-04-19 18:41:36 +0200

    Added  repetable menu

--- consumer/src/main/java/consumer/Main.java
+++ consumer/src/main/java/consumer/Main.java
@@ -20,35 +20,39 @@ public class Main {
                 imperial = e.getClass().getAnnotation(NamingAnnotation.class).name();
             }
         }
-
-
-        System.out.println("Welcome to the Bmi Calculator \nPlease enter select if you want to use Metric or Imperial units");
-        System.out.println("1. "+ imperial+"\n2. " + metric);
-        Scanner sc = new Scanner(System.in);
-        String choice = sc.nextLine();
-        if(choice.equals("1")){
-            System.out.println("Enter your height in full inches");
-            String height = sc.nextLine();
-            System.out.println("Enter your weight in full pounds");
-            String weight = sc.nextLine();
-            for(BmiCalculate g : ServiceLoader.load(BmiCalculate.class)) {
-                if(g.getClass().getSimpleName().equals("ImperialBmiCalculator")) {
-                    System.out.println(g.getResult(Integer.parseInt(height), Integer.parseInt(weight)));
+        System.out.println("Welcome to the Bmi Calculator");
+        while(true) {
+            System.out.println("Please enter select if you want to use Metric or Imperial units");
+            System.out.println("1. "+ imperial+"\n2. " + metric + "\n3. Exit");
+            Scanner sc = new Scanner(System.in);
+            String choice = sc.nextLine();
+            if(choice.equals("1")){
+                System.out.println("Enter your height in full inches");
+                String height = sc.nextLine();
+                System.out.println("Enter your weight in full pounds");
+                String weight = sc.nextLine();
+                for(BmiCalculate g : ServiceLoader.load(BmiCalculate.class)) {
+                    if(g.getClass().getSimpleName().equals("ImperialBmiCalculator")) {
+                        System.out.println(g.getResult(Integer.parseInt(height), Integer.parseInt(weight)));
+                    }
                 }
-            }
-        } else if(choice.equals("2")){
-            System.out.println("Enter your height in cm");
-            String height = sc.nextLine();
-            System.out.println("Enter your weight in kg");
-            String weight = sc.nextLine();
-            for(BmiCalculate g : ServiceLoader.load(BmiCalculate.class)) {
-                if(g.getClass().getSimpleName().equals("MetricBmiCalculator")) {
-                    System.out.println(g.getResult(Integer.parseInt(height), Integer.parseInt(weight)));
+            } else if(choice.equals("2")){
+                System.out.println("Enter your height in cm");
+                String height = sc.nextLine();
+                System.out.println("Enter your weight in kg");
+                String weight = sc.nextLine();
+                for(BmiCalculate g : ServiceLoader.load(BmiCalculate.class)) {
+                    if(g.getClass().getSimpleName().equals("MetricBmiCalculator")) {
+                        System.out.println(g.getResult(Integer.parseInt(height), Integer.parseInt(weight)));
+                    }
                 }
+            } else if (choice.equals("3")) {
+                break;
+            } else {
+                System.out.println("Wrong input");
             }
-        } else {
-            System.out.println("Wrong input");
         }
+
 
     }
 
